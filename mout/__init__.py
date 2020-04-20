efabffd86e205963da6fdb7b29d7536fce2ac2ca
Max Winokan <mwinokan@me.com>
2020-04-20 14:09:40 +0100

    Added convert.py and headerOut

--- mout/__init__.py
+++ mout/__init__.py
@@ -1,9 +1,10 @@
 from .output import out
 from .output import varOut
+from .output import headerOut
 from .output import warningOut
 from .output import errorOut
-from .output import toPrecision # documentation missing
-
-from .ase import printEnergy # to be moved to AMP
 
 from .progress import progress
+
+from .convert import toPrecision # documentation missing
+from .convert import str2bool # documentation missing
