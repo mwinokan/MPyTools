4ecf92a56a6df80524f80cb63f420013b6edddd3
mwinokan <mwinokan@me.com>
2020-04-28 13:17:17 +0200

    Fixed issues with custom colour cycler.

--- mplot/__init__.py
+++ mplot/__init__.py
@@ -2,7 +2,7 @@
 # setting colours
 import matplotlib as mpl
 from cycler import cycler
-mpl.rcParams['axes.prop_cycle'] = cycler(color=['#e91c00','#007dbe','#00A87C',"EAA900","A000DE","5FBDEF","F2E82D","FF0000","0700FF","00FF00","FFAE00","F18CF4","00FFFF"])
+mpl.rcParams['axes.prop_cycle'] = cycler(color=['#e91c00','#007dbe','#00A87C',"#EAA900","#A000DE","#5FBDEF","#F2E82D","#FF0000","#0700FF","#00FF00","#FFAE00","#F18CF4","#00FFFF"])
 
 from .main import show
 
