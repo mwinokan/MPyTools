0b1bec7658bfe4580a172d068ee782a5d2f0ce7f
mwinokan <mwinokan@me.com>
2020-06-08 13:00:04 +0200

    added closeAll

--- mplot/main.py
+++ mplot/main.py
@@ -6,13 +6,24 @@ import matplotlib
 matplotlib.use("tkagg")
 import matplotlib.pyplot as plt
 
-def show(verbosity=1):
+def showAll(verbosity=1):
   if (verbosity > 0):
-    mout.out("showing graphs"+" ... ",
+    mout.out("showing all graphs"+" ... ",
              printScript=True,
              end='')
 
   plt.show()
+
+  if (verbosity > 0):
+    mout.out("Done.")
+
+def closeAll(verbosity=1):
+  if (verbosity > 0):
+    mout.out("closing all graphs"+" ... ",
+             printScript=True,
+             end='')
+
+  plt.close(fig='all')
 
   if (verbosity > 0):
     mout.out("Done.")
