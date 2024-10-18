a4f4e96cf5d895da48704a6f6470ecced03ffe67
Max Winokan <mwinokan@me.com>
2024-10-18 10:38:06 +0100

    tools for prefix stripping

---
+++ mrich/tools.py
@@ -0,0 +1,33 @@
+
+import re
+
+def detect_format_prefix(message):
+
+    match = re.match(f"^\[.*?\]", message)
+
+    if match:
+        match = match.group()
+
+    return match
+
+def strip_formats(*messages, text="", separator=" "):
+    formats = []
+    start = len(text) + 1
+    for message in messages:
+        prefix = detect_format_prefix(message)
+
+        prefix_found = prefix is not None
+
+        if prefix_found:
+            message = message.removeprefix(prefix)
+            prefix = prefix.removeprefix('[').removesuffix(']')
+
+        end = start + len(message)
+        text += separator + message
+        
+        if prefix_found:
+            formats.append((prefix, start, end+2))
+            
+        start = end
+
+    return text, formats
