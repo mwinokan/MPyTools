0d5969403b3a9007e716d83d0b4839d6938c9343
Max Winokan <mwinokan@me.com>
2024-12-09 16:03:06 +0000

    fix set_progress_* when none is active

--- mrich/wrappers.py
+++ mrich/wrappers.py
@@ -1,5 +1,6 @@
 from .console import console, console_print
 
+
 # wrappers
 def print(*args, **kwargs):
     console_print(*args, **kwargs)
@@ -12,19 +13,29 @@ def out(*args, **kwargs):
 def rule(*args, **kwargs):
     console.rule(*args, **kwargs)
 
+
 ### PROGRESS
 
 CURRENT_PROGRESS = None
+
 
 def clear_progress():
     global CURRENT_PROGRESS
     CURRENT_PROGRESS = None
 
+
 def track(*args, prefix: str = "Working...", **kwargs):
-    
+
     from .colors import THEME
     from rich.console import Console
-    from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn, TimeElapsedColumn
+    from rich.progress import (
+        Progress,
+        TextColumn,
+        BarColumn,
+        TaskProgressColumn,
+        TimeRemainingColumn,
+        TimeElapsedColumn,
+    )
 
     global CURRENT_PROGRESS
 
@@ -44,26 +55,35 @@ def track(*args, prefix: str = "Working...", **kwargs):
     # CURRENT_PROGRESS.tasks[0].fields["suffix"] = ""
 
     with CURRENT_PROGRESS:
-        yield from CURRENT_PROGRESS.track(
-            *args, description=prefix, **kwargs
-        )
-    
-def set_progress_field(key: str = None, value = None, **kwargs):
+        yield from CURRENT_PROGRESS.track(*args, description=prefix, **kwargs)
+
+
+def set_progress_field(key: str = None, value=None, **kwargs):
     progress = CURRENT_PROGRESS
+
+    if not progress:
+        return
+
     task = progress.tasks[0]
-    
+
     if kwargs:
-        for key,value in kwargs.items():
+        for key, value in kwargs.items():
             task.fields[key] = value
     else:
         task.fields[key] = value
 
+
 def set_progress_prefix(text):
     progress = CURRENT_PROGRESS
+
+    if not progress:
+        return
+
     task = progress.tasks[0]
     task.description = text
 
+
 # def set_progress_suffix(text):
 #     set_progress_field("suffix", text)
-    # task = progress.tasks[0]
-    # task.description = text
+# task = progress.tasks[0]
+# task.description = text
